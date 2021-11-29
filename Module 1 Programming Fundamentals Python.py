3fb3e8fb796a37f1bfbc63f1cc4a2ad4c140e3ed
Simon Lobascher <s.lobascher@gmail.com>
2021-11-29 16:41:05 +1100

    SIMON1

---
+++ Module 1 Programming Fundamentals Python.py
@@ -0,0 +1,40 @@
+#  Part 1: Data types
+'''
+int 54, 56, 56, 
+
+float 54.3, 45.3333
+
+str "hey idiot"
+
+None 
+'''
+
+print(type(54))
+
+#  Part 2: Comments
+"""
+print("do a weeeeeee") #  don't actually though
+
+"""
+
+print (54) #  Dont do this
+
+'''
+print("yeah yeah yeah")
+'''
+
+#  Part 3: Variables and printing
+print("poopoo", end= " | ")
+print ("Simon")
+
+x = 3
+y = "hello"
+num = y
+y = x
+
+print (x, num)
+
+max_score = 10
+
+print (max_score)
+
